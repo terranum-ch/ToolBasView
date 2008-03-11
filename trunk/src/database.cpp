8403ab8a4032256964fe5aeaa0ca0a124a370776
lucsch <lucsch@879b45be-c703-4f91-bc5e-d7ec0cb4b60f>
2008-03-11 15:29:29 +0000

    modification of database.cpp for new home made mysql library

--- trunk/src/database.cpp
+++ trunk/src/database.cpp
@@ -81,7 +81,7 @@ bool DataBase::DataBaseOpen (wxString path, enum Lang_Flag flag)
 		"this_program",       /* this string is not used */
 		stemps,
 		"--language=./share/english",
-		"--skip-plugin-innodb",
+		//"--skip-plugin-innodb", // not needed if home made libraray
 		"--port=3309",
 		"--character-sets-dir=./share/charsets",
 		"--default-character-set=utf8"
@@ -547,7 +547,7 @@ bool DataBase::DataBaseCreateNew(wxString DataBasePath, wxString DataBaseName,en
 		"this_program",       /* this string is not used */
 		stemps,
 		"--language=./share/english",
-		"--skip-plugin-innodb",//"--skip-innodb", // dosen't exist in 5.1 --> lead to a crash
+		//"--skip-plugin-innodb",// dosen't exist in 5.1 --> lead to a crash // not needed if home-made
 		"--port=3309",
 		"--character-sets-dir=./share/charsets",
 		"--character_set_server=utf8"
