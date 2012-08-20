a5b93bad3839f72f87928f93737600dc256bb38f
lucsch <lucsch@879b45be-c703-4f91-bc5e-d7ec0cb4b60f>
2012-08-20 10:17:24 +0000

    Updating configure script for Windows

--- trunk/build/scripts/update-toolbasview.py
+++ trunk/build/scripts/update-toolbasview.py
@@ -8,7 +8,10 @@ Copyright (c) 2012 . All rights reserved.
 """
 
 import os
-from Tkinter import *
+try:
+    from Tkinter import *
+except :
+    from tkinter import *
 from subprocess import *
 
 def GetCmakeListPath():
@@ -31,11 +34,21 @@ def BuildMacBook():
     print ("Configuring MacBook")
     builddir = "/Users/lucien/DATA/PROGRAMATION/toolbasview/bin"
     try:
-        p = Popen("cmake -GXcode " + GetCmakeListPath() + "  -DCMAKE_OSX_ARCHITECTURES:TEXT=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:TEXT=10.6 -DCMAKE_WXWINDOWS_WXCONFIG_EXECUTABLE:FILE=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBWXSVN/bin/wx-config -DMYSQL_MAIN_DIR:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBMYSQL -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS -DSEARCH_GEOS_LIB_PATH:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS", shell=True, cwd=builddir)
+        p = Popen("cmake -G\"Visual Studio 10\" " + GetCmakeListPath() + "-DwxWIDGETS_USING_SVN:BOOL=1  -DwxWIDGETS_PATH_SVN:PATH=D:\\LIB\\wxWIDGETS-SVN -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=D:\\LIB\\LIB_GDAL  -DSEARCH_GEOS_LIB_PATH:PATH=D:\\LIB\\geos-3.3.4 -DSQLITE_PATH:PATH=D:\\LIB\\LIB_SQLITE -DUSE_MT_LIBRARY:BOOL=1", shell=True, cwd=builddir)
         p.wait()
     except:
         print("Error creating makefile")
 
+
+
+def BuildWindows7():
+    print ("Configuring Windows")
+    builddir = "D:\\PROGRAMMATION\\ToolBasView\\bin"
+    try:
+        p = Popen("cmake -G\"Visual Studio 10\" " + GetCmakeListPath() + " -DwxWIDGETS_USING_SVN:BOOL=1  -DwxWIDGETS_PATH_SVN:PATH=D:\\LIB\\wxWIDGETS-SVN -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=D:\\LIB\\LIB_GDAL  -DSEARCH_GEOS_LIB_PATH:PATH=D:\\LIB\\geos-3.3.4 -DSQLITE_PATH:PATH=D:\\LIB\\LIB_SQLITE -DUSE_MT_LIBRARY:BOOL=1 -DMYSQL_MAIN_DIR:PATH=D:\\LIB\\LIB_MYSQL", shell=True, cwd=builddir)
+        p.wait()
+    except:
+        print("Error creating makefile")
 
 if __name__ == '__main__':
 
@@ -51,6 +64,8 @@ if __name__ == '__main__':
     button1 = Button(myContainer1, text="Configure Mac Pro (UNIL)", command=BuildMacPro)
     button1.pack()
 
+    button3 = Button(myContainer1, text= "Configure Windows 7", command=BuildWindows7)
+    button3.pack()
 
     root.mainloop()
 
