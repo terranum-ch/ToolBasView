c706401a0bd994f58fc72670437ddd8646d37ab8
lucsch <lucsch@879b45be-c703-4f91-bc5e-d7ec0cb4b60f>
2013-02-12 07:35:13 +0000

    Improve build script

--- trunk/build/scripts/update-toolbasview.py
+++ trunk/build/scripts/update-toolbasview.py
@@ -9,62 +9,90 @@ Copyright (c) 2012 . All rights reserved.
 
 import os
 try:
-    from Tkinter import *
+  from Tkinter import *
 except :
-    from tkinter import *
+  from tkinter import *
 from subprocess import *
 
-def GetCmakeListPath():
-    myFileName = os.path.split(__file__)
-    myCmakePath = os.path.dirname(myFileName[0])
-    return myCmakePath
+
+def GetCmakeDirName():
+  #print ('sys.argv[0] =', sys.argv[0])
+  pathname = os.path.dirname(sys.argv[0])        
+  return os.path.abspath(pathname)
+
+
+def updateSVN():
+  myPath = os.path.normpath(os.path.join(GetCmakeDirName(), ".."))
+  print ("Updating subversion in: {}".format(myPath))
+  try:
+    p = Popen("svn update", shell=True, cwd = myPath)
+    p.wait()
+  except:
+    print ("Error updating subversion in: {}".format(myPath))
+
+  # print version number
+  try:
+    p = Popen("svnversion", shell=True, cwd=myPath, stdout=PIPE, stderr=PIPE)
+    information = p.communicate()
+    myVersion = str(information[0])
+    myVersionAlone = myVersion[myVersion.find("'")+1:-3] 
+    print ("Updated version is: {}".format(myVersionAlone))
+  except:
+    print("Error getting version for {}".format(GetCmakeDirName()))
 
 
 def BuildMacPro():
-    print ("Configuring Mac Pro (UNIL)")
-    builddir = "/Users/lucien/Documents/PRJ/TOOLBASVIEW/bin"
-    try:
-        p = Popen("cmake -GXcode " + GetCmakeListPath() + "  -DCMAKE_OSX_ARCHITECTURES:TEXT=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:TEXT=10.6 -DCMAKE_WXWINDOWS_WXCONFIG_EXECUTABLE:FILE=/Users/lucien/Documents/PROGRAMMATION/64/_LIBWXSVN/bin/wx-config -DMYSQL_MAIN_DIR:PATH=/Users/lucien/Documents/PROGRAMMATION/64/_LIBMYSQL -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=/Users/lucien/Documents/PROGRAMMATION/64/_LIBGIS -DSEARCH_GEOS_LIB_PATH:PATH=/Users/lucien/Documents/PROGRAMMATION/64/_LIBGIS", shell=True, cwd=builddir)
-        p.wait()
-    except:
-        print("Error creating makefile")
+  print ("Configuring Mac Pro (UNIL)")
+  builddir = "/Users/lucien/Documents/PRJ/TOOLBASVIEW/bin"
+  try:
+    p = Popen("cmake -GXcode " + os.path.join(GetCmakeDirName(), "..") + "  -DCMAKE_OSX_ARCHITECTURES:TEXT=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:TEXT=10.6 -DCMAKE_WXWINDOWS_WXCONFIG_EXECUTABLE:FILE=/Users/lucien/Documents/PROGRAMMATION/64/_LIBWXSVN/bin/wx-config -DMYSQL_MAIN_DIR:PATH=/Users/lucien/Documents/PROGRAMMATION/64/_LIBMYSQL -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=/Users/lucien/Documents/PROGRAMMATION/64/_LIBGIS -DSEARCH_GEOS_LIB_PATH:PATH=/Users/lucien/Documents/PROGRAMMATION/64/_LIBGIS", shell=True, cwd=builddir)
+    p.wait()
+  except:
+    print("Error creating makefile")
 
 
 def BuildMacBook():
-    print ("Configuring MacBook")
-    builddir = "/Users/lucien/DATA/PROGRAMATION/toolbasview/bin"
-    try:
-        p = Popen("cmake -GXcode " + GetCmakeListPath() + "  -DCMAKE_OSX_ARCHITECTURES:TEXT=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:TEXT=10.6 -DCMAKE_WXWINDOWS_WXCONFIG_EXECUTABLE:FILE=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBWXSVN/bin/wx-config -DMYSQL_MAIN_DIR:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBMYSQL -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS -DSEARCH_GEOS_LIB_PATH:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS", shell=True, cwd=builddir)
-    except:
-        print("Error creating makefile")
+  print ("Configuring MacBook")
+  builddir = "/Users/lucien/DATA/PROGRAMATION/toolbasview/bin"
+  try:
+    p = Popen("cmake -GXcode " + os.path.join(GetCmakeDirName(), "..") + "  -DCMAKE_OSX_ARCHITECTURES:TEXT=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET:TEXT=10.6 -DCMAKE_WXWINDOWS_WXCONFIG_EXECUTABLE:FILE=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBWXSVN/bin/wx-config -DMYSQL_MAIN_DIR:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBMYSQL -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS -DSEARCH_GEOS_LIB_PATH:PATH=/Users/lucien/DATA/PROGRAMATION/_LIB/64/_LIBGIS", shell=True, cwd=builddir)
+  except:
+    print("Error creating makefile")
 
 
 
 def BuildWindows7():
-    print ("Configuring Windows")
-    builddir = "D:\\PROGRAMMATION\\ToolBasView\\bin"
-    try:
-        p = Popen("cmake -G\"Visual Studio 10\" " + GetCmakeListPath() + " -DwxWIDGETS_USING_SVN:BOOL=1  -DwxWIDGETS_PATH_SVN:PATH=D:\\LIB\\wxWIDGETS-SVN -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=D:\\LIB\\LIB_GDAL  -DSEARCH_GEOS_LIB_PATH:PATH=D:\\LIB\\geos-3.3.5 -DUSE_MT_LIBRARY:BOOL=1 -DMYSQL_MAIN_DIR:PATH=D:\\LIB\\LIB_MYSQL", shell=True, cwd=builddir)
-        p.wait()
-    except:
-        print("Error creating makefile")
+  print ("Configuring Windows")
+  builddir = "D:\\PROGRAMMATION\\ToolBasView\\bin"
+  try:
+    p = Popen("cmake -G\"Visual Studio 10\" " + os.path.join(GetCmakeDirName(), "..") + " -DwxWIDGETS_USING_SVN:BOOL=1  -DwxWIDGETS_PATH_SVN:PATH=D:\\LIB\\wxWIDGETS-SVN -DSEARCH_GDAL:BOOL=1 -DSEARCH_GEOS:BOOL=1 -DSEARCH_GDAL_LIB_PATH:PATH=D:\\LIB\\LIB_GDAL  -DSEARCH_GEOS_LIB_PATH:PATH=D:\\LIB\\geos-3.3.5 -DUSE_MT_LIBRARY:BOOL=1 -DMYSQL_MAIN_DIR:PATH=D:\\LIB\\LIB_MYSQL", shell=True, cwd=builddir)
+    p.wait()
+  except:
+    print("Error creating makefile")
 
 if __name__ == '__main__':
 
-    root = Tk()
-    myContainer1 = Frame(root)
-    myContainer1.pack()
+  root = Tk()
+  myContainer1 = Frame(root)
+  myContainer1.pack()
+  
+  label = Label(myContainer1, text="TOOLBASVIEW")
+  label.pack()
 
-    label = Label(myContainer1)
 
-    button2 = Button(myContainer1, text="Configure MacBook", command=BuildMacBook)
-    button2.pack()
 
-    button1 = Button(myContainer1, text="Configure Mac Pro (UNIL)", command=BuildMacPro)
-    button1.pack()
+  label = Label(myContainer1)
 
-    button3 = Button(myContainer1, text= "Configure Windows 7", command=BuildWindows7)
-    button3.pack()
+  button3 = Button(myContainer1, text=("Update subversion"), command=updateSVN)
+  button3.pack()
 
-    root.mainloop()
+  button2 = Button(myContainer1, text="Configure MacBook", command=BuildMacBook)
+  button2.pack()
+
+  button1 = Button(myContainer1, text="Configure Mac Pro (UNIL)", command=BuildMacPro)
+  button1.pack()
+  button3 = Button(myContainer1, text= "Configure Windows 7", command=BuildWindows7)
+  button3.pack()
+
+  root.mainloop()
 
